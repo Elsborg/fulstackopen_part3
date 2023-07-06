a364deaa2a37ac1bce2f26a462cd7d76fef387fc
elsborg <martin.elsborg@gmail.com>
2023-07-06 12:12:42 +0200

    dotenv added

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      req.method === 'POST' ? JSON.stringify(req.body) : ''
-    ].join(' ')
-  }))
+
+morgan.token('payload', function (request) {
+    return JSON.stringify(request.body)
+})
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :payload"))
 
 app.use(cors())
 
